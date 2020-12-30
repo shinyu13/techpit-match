f4c98487e192db15b722b10e39633adef8767772
shinyup213 <shinya@yup.jp>
2020-12-30 21:16:45 +0900

    first commit

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("ChatRoomChannel", {
+const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
